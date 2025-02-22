f8060943b6c60a61ba9c444d4bd3fa4dc54a7b44
Scotty <73749875+intondiscott@users.noreply.github.com>
2025-02-22 22:16:34 +0000

    uploaded files

---
+++ index.js
@@ -0,0 +1,42 @@
+toggleMenu = () => {
+  document.querySelector(".burger-menu").classList.toggle("open");
+};
+
+nightMode = () => {
+  document.querySelector("body").classList.toggle("dark");
+};
+
+const getData = async () => {
+  const data = await fetch("./projects.json");
+  const response = await data.json();
+  return response.projects;
+};
+
+const returnedData = async () => {
+  const data = await getData();
+  project = document.querySelector(".projects");
+
+  data.map((item) => {
+    projectContainer = document.createElement("div");
+    projectName = document.createElement("h3");
+    projectImage = document.createElement("img");
+    projectDescription = document.createElement("p");
+    projectButton = document.createElement("button");
+    projectLink = document.createElement("a");
+    projectContainer.classList.add("project");
+    projectName.innerHTML = item.name;
+    projectImage.src = item.image;
+    projectDescription.innerHTML = item.description;
+    projectLink.href = item.codeLink;
+    projectButton.innerHTML = "View Code";
+    projectLink.appendChild(projectButton);
+
+    projectContainer.appendChild(projectName);
+    projectContainer.appendChild(projectImage);
+    projectContainer.appendChild(projectDescription);
+    projectContainer.appendChild(projectLink);
+    project.appendChild(projectContainer);
+  });
+};
+
+returnedData();
