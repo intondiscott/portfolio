e9a1f7641653c15e78a90b93c44a8fa2a90943b6
intondiscott <intondiscott@gmail.com>
2025-02-26 19:22:36 -0500

    updated UI

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const returnedData = async () => {
     projectContainer = document.createElement("div");
     projectName = document.createElement("h3");
     projectImage = document.createElement("img");
+    projectIconGithub = document.createElement("img");
     projectDescription = document.createElement("p");
     projectButton = document.createElement("button");
     projectLink = document.createElement("a");
@@ -28,8 +29,10 @@ const returnedData = async () => {
     projectImage.src = item.image;
     projectDescription.innerHTML = item.description;
     projectLink.href = item.codeLink;
-    projectButton.innerHTML = "View Code";
-    projectLink.appendChild(projectButton);
+    projectIconGithub.src = "./assets/github-mark.png";
+    projectIconGithub.classList.add("icon-github");
+
+    projectLink.appendChild(projectIconGithub);
 
     projectContainer.appendChild(projectName);
     projectContainer.appendChild(projectImage);
